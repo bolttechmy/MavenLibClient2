110898e37b4df3b25ea004a261e057cc1f45ad7b
anand kumar <anand.kumar@puresoftware.com>
2022-05-18 16:59:54 +0530

    first commit

---
+++ app/src/main/java/com/example/mavenlibclient2/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mavenlibclient2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mavenlib2.MavenLibMainActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(
+            Intent(
+                this@MainActivity, MavenLibMainActivity::class.java
+            )
+        )
+    }
+}
---
+++ mavenlib2/src/main/java/com/example/mavenlib2/MavenLibMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mavenlib2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MavenLibMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maven_lib_main)
+    }
+}
