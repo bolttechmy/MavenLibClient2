6f09658823acd8ac88fbcc2bd10fdc8e41bc01c4
anand kumar <anand.kumar@puresoftware.com>
2022-05-20 13:46:22 +0530

    changes related to manifest

--- app/src/main/java/com/example/mavenlibclient2/MainActivity.kt
+++ app/src/main/java/com/example/mavenlibclient2/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.mavenlibclient2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.mavenlib2.MavenLibMainActivity
+
 //import com.example.mavenlib2.MavenLibMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        startActivity(
-            Intent(
-                this@MainActivity, MainActivity::class.java
+        findViewById<TextView>(R.id.tvTest).setOnClickListener{
+            startActivity(
+                Intent(
+                    this@MainActivity, MavenLibMainActivity::class.java
+                )
             )
-        )
+        }
     }
 }
